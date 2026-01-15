85cdd9aad4e138e6ef94b7c582a48cae36809e53
Yusmvv <wallmeet1@gmail.com>
2026-01-15 20:36:07 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js - ФАЙЛ С СЕКРЕТАМИ! НИКОГДА НЕ ДЕЛАЙТЕ ЕГО ПУБЛИЧНЫМ!
+// 1. Замените значения ниже на свои
+// 2. Убедитесь, что этот файл добавлен в .gitignore
+window.CONFIG = {
+    GIST_ID: 'e3385a52a9d11e223c70cb60a346485e',    // Вставьте ваш реальный Gist ID
+    GITHUB_TOKEN: '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'             // Вставьте ваш новый GitHub токен
+};
